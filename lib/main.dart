9963e942b60b5166bd2398cdb74ea1ea69592e8c
Amiir <amirlearn08@gmail.com>
2024-03-04 04:20:02 -0800

    finsihed the ludo app

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,123 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    home: MyWidget(),
   ));
 }
 
 class Home extends StatelessWidget {
-  const Home({super.key});
-
+  
+   Home({super.key});
+  // int Ludo  = 1;
+   int ludoNumber  = 3;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-        title: Text(
-          'Products',
-          style: TextStyle(
-              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+        appBar: AppBar(
+          elevation: 16,
+          backgroundColor: Color(0xff000000),
+          centerTitle: true,
+          title: Text(
+            'Products',
+            style: TextStyle(
+                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
         ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: ListTile(
-              title: Text('Product Name'),
-              subtitle: Text("From China"),
-              trailing: IconButton(onPressed: (){}, icon : Icon(Icons.add)),
-              leading: Image.network(
-                  "https://images.pexels.com/photos/3735655/pexels-photo-3735655.jpeg?auto=compress&cs=tinysrgb&w=600"),
-            ),
+        backgroundColor: Colors.red,
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: InkWell(
+                    onDoubleTap: () {
+                      print('You double clicked me ');
+                    },
+                    onTap: () {
+                      print('Hello word');
+                    },
+                    child: Image.asset('images/dice$ludoNumber.png')),
+              )),
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                    onDoubleTap: () {
+                      print('double clicked me');
+                    },
+                    onTap: () {
+                      print('You clicked me');
+                    },
+                    child: Image.asset('images/dice1.png')),
+              )),
+            ],
           ),
-          Center(
-            child: ListTile(
-              title: Text('Product Name'),
-              subtitle: Text("From China"),
-              trailing: IconButton(onPressed: (){}, icon : Icon(Icons.add)),
-              leading: Image.network(
-                  "https://images.pexels.com/photos/3735655/pexels-photo-3735655.jpeg?auto=compress&cs=tinysrgb&w=600"),
-            ),
+        ));
+  }
+}
+
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  int ludoNumber =2 ;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 16,
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+          title: Text(
+            'Ludo Generating',
+            style: TextStyle(
+                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
           ),
-          Center(
-            child: ListTile(
-              title: Text('Product Name'),
-              subtitle: Text("From China"),
-              trailing: IconButton(onPressed: (){}, icon : Icon(Icons.add)),
-              leading: Image.network(
-                  "https://images.pexels.com/photos/3735655/pexels-photo-3735655.jpeg?auto=compress&cs=tinysrgb&w=600"),
-            ),
+        ),
+        backgroundColor: Colors.white60,
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: InkWell(
+                    onDoubleTap: () {
+                      setState(() {
+                        ludoNumber = new Random().nextInt(6)+1;
+                      });
+                    },
+                    onTap: () {
+                      setState(() {
+                        ludoNumber = new Random().nextInt(6)+1;
+                      });
+                    },
+                    child: Image.asset('images/dice$ludoNumber.png')),
+              )),
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: InkWell(
+                    onDoubleTap: () {
+                      print('double clicked me');
+                    },
+                    onTap: () {
+                    setState(() {
+                      ludoNumber =  new Random().nextInt(6)+1;
+                    });
+                    },
+                    child: Image.asset('images/dice$ludoNumber.png')),
+              )),
+            ],
           ),
-          Center(
-            child: ListTile(
-              title: Text('Product Name'),
-              subtitle: Text("From China"),
-              trailing: IconButton(onPressed: (){}, icon : Icon(Icons.add)),
-              leading: Image.network(
-                  "https://images.pexels.com/photos/3735655/pexels-photo-3735655.jpeg?auto=compress&cs=tinysrgb&w=600"),
-            ),
-          ),
-          Center(
-            child: ListTile(
-              title: Text('Product Name'),
-              subtitle: Text("From China"),
-              trailing: IconButton(onPressed: (){}, icon : Icon(Icons.add)),
-              leading: Image.network(
-                  "https://images.pexels.com/photos/3735655/pexels-photo-3735655.jpeg?auto=compress&cs=tinysrgb&w=600"),
-            ),
-          ),
-          Center(
-            child: ListTile(
-              title: Text('Product Name'),
-              subtitle: Text("From China"),
-              trailing: IconButton(onPressed: (){}, icon : Icon(Icons.add)),
-              leading: Image.network(
-                  "https://images.pexels.com/photos/3735655/pexels-photo-3735655.jpeg?auto=compress&cs=tinysrgb&w=600"),
-            ),
-          ),
-          Center(
-            child: ListTile(
-              title: Text('Product Name'),
-              subtitle: Text("From China"),
-              trailing: IconButton(onPressed: (){}, icon : Icon(Icons.add)),
-              leading: Image.network(
-                  "https://images.pexels.com/photos/3735655/pexels-photo-3735655.jpeg?auto=compress&cs=tinysrgb&w=600"),
-            ),
-          ),
-          Container(
-            width: 400,
-            padding: EdgeInsets.only(left: 10,right: 10),
-            margin: EdgeInsets.all(10),
-            child: ElevatedButton(
-              
-              onPressed:(){}, child:Text('Add To Cart'),),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
